f23828871e5e364f3c70b4beb2fbfcf7a4a12069
Carbon.lv <130360750+carbon-portkey@users.noreply.github.com>
2024-02-29 17:14:18 +0800

    feat: update

--- Schrodinger/src/utils/googleLogin.ts
+++ Schrodinger/src/utils/googleLogin.ts
@@ -7,12 +7,17 @@ import { AccessTokenRequest, makeRedirectUri, AuthRequest } from 'expo-auth-sess
 const Config = {
   GOOGLE_IOS_CLIENT_ID: '183226380326-38oi8hev1fug9js9gpbtdicgqgb81a78.apps.googleusercontent.com',
   GOOGLE_ANDROID_CLIENT_ID: '183226380326-7ov54dl0p2noc8j9cmvl5c1s0e770f5u.apps.googleusercontent.com',
+  GOOGLE_WEB_CLIENT_ID: '183226380326-ei86f6dh7v541u6m8c5karu57g00mu56.apps.googleusercontent.com',
 };
 
 const googleLogin = async () => {
-  const request = new AuthRequest({ clientId: Config.GOOGLE_IOS_CLIENT_ID ?? '', redirectUri: makeRedirectUri({
-    native: `${Application.applicationId}:/oauthredirect`,
-  }), scopes: ['openid', 'profile', 'email']});
+  const request = new AuthRequest({
+    clientId: Config.GOOGLE_IOS_CLIENT_ID ?? '',
+    redirectUri: makeRedirectUri({
+      native: `${Application.applicationId}:/oauthredirect`,
+    }),
+    scopes: ['openid', 'profile', 'email'],
+  });
   const iosPromptAsync: () => Promise<string> = async () => {
     const info = await request.promptAsync(Google.discovery);
     if (info.type === 'success') {
@@ -37,6 +42,10 @@ const googleLogin = async () => {
 
   const androidPromptAsync = async () => {
     try {
+      GoogleSignin.configure({
+        offlineAccess: true,
+        webClientId: Config.GOOGLE_WEB_CLIENT_ID,
+      });
       await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
       // google services are available
     } catch (err) {
