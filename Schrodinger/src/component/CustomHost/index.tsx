a4af8d4890d5fda89de931cd86cadae5d42015be
wade-portkey <codingwizard@portkey.finance>
2024-03-19 17:16:06 +0800

    feat: 🎸 fix release package crash

--- Schrodinger/src/component/CustomHost/index.tsx
+++ Schrodinger/src/component/CustomHost/index.tsx
@@ -3,26 +3,28 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const CustomHostStorageKey = "ai.schrodingernft.hostUrl";
 
-DevSettings.addMenuItem('Custom Host Url', async () => {
-  const hostUrl = await AsyncStorage.getItem(CustomHostStorageKey);
-  Alert.prompt(
-    "Enter host url",
-    "Enter host url such as 'https://schrodingernft.ai/'",
-    [
-      {
-        text: "Cancel",
-        onPress: () => console.log("Cancel Pressed"),
-        style: "cancel"
-      },
-      {
-        text: "OK",
-        onPress: text => {
-          AsyncStorage.setItem(CustomHostStorageKey, text ?? '');
-          Alert.alert('Host url updated, please restart the app to take effect');
+if (__DEV__) {
+  DevSettings.addMenuItem('Custom Host Url', async () => {
+    const hostUrl = await AsyncStorage.getItem(CustomHostStorageKey);
+    Alert.prompt(
+      "Enter host url",
+      "Enter host url such as 'https://schrodingernft.ai/'",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        {
+          text: "OK",
+          onPress: text => {
+            AsyncStorage.setItem(CustomHostStorageKey, text ?? '');
+            Alert.alert('Host url updated, please restart the app to take effect');
+          }
         }
-      }
-    ],
-    "plain-text",
-    hostUrl ?? "https://schrodingernft.ai/"
-  );
-});
+      ],
+      "plain-text",
+      hostUrl ?? "https://schrodingernft.ai/"
+    );
+  });
+}
